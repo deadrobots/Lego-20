f67699799ba56f769491437d0c1b20c6f3bcf50d
Lexie <charlie.clyne@yahoo.com>
2020-02-14 21:35:15 -0500

    consolidated init, cleaned up the drive to bin, drop bin, and ramp drive.

--- actions.py
+++ actions.py
@@ -10,6 +10,27 @@ def init():
     enable_servos()
     print("don't touch me, i'm calibrating!!")
     g.calibrate_gyro()
+    # power_on_test()
+    u.move_servo(c.LEFT_ARM, c.LA_FRONT)
+    u.wait_for_button()
+
+
+def power_on_test():
+    # testing servos
+    print("testing front arm")
+    u.move_servo(c.FRONT_ARM, c.FA_UP)
+    u.move_servo(c.FRONT_ARM, c.FA_MID)
+    u.move_servo(c.FRONT_ARM, c.FA_UP)
+    print("testing claw")
+    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
+    u.move_servo(c.FRONT_CLAW, c.FC_OPEN)
+    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
+    print("testing left arm")
+    u.move_servo(c.LEFT_ARM, c.LA_FRONT)
+    u.move_servo(c.LEFT_ARM, c.LA_BACK)
+    u.move_servo(c.LEFT_ARM, c.LA_FRONT)
+    #testing switch
+    u.wait_for_switch(c.ARM_SWITCH)
     # testing motors
     print("testing motors")
     g.drive_timed(30, 1000)
@@ -21,19 +42,6 @@ def init():
         print("i am lego prime")
     else:
         print("i am lego clone")
-    # testing servos
-    print("testing left arm")
-    u.move_servo(c.LEFT_ARM, c.LA_FRONT)
-    u.move_servo(c.LEFT_ARM, c.LA_BACK)
-    u.move_servo(c.LEFT_ARM, c.LA_FRONT)
-    print("testing front arm")
-    u.move_servo(c.FRONT_ARM, c.FA_UP)
-    u.move_servo(c.FRONT_ARM, c.FA_MID)
-    u.move_servo(c.FRONT_ARM, c.FA_UP)
-    print("testing left arm")
-    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
-    u.move_servo(c.FRONT_CLAW, c.FC_OPEN)
-    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
     # testing tophat
     print("testing right tophat")
     g.drive_condition(50, d.on_white_right)
@@ -51,44 +59,46 @@ def init():
     msleep(500)
     print("see white")
     g.drive_timed(-50, 500)
-    u.wait_for_button()
     # g.calibrate_gyro()
 
 
 def lower_ramp():
-    g.drive_distance(60, 18)
+    g.drive_distance(60, 23)
     msleep(100)
     set_servo_position(c.LEFT_ARM, c.LA_SIDE)
     msleep(250)
-    g.drive_distance(-60, 9)
+    g.drive_distance(-60, 13)  #9
     msleep(100)
 
 
 def move_coupler_to_blocks():
     print "move coupler"
-    g.pivot_on_left_wheel(50, 100)
+    g.pivot_on_left_wheel(80, 70)
     d.line_follow_right(1500)
     msleep(100)
     d.cross_line()
     msleep(50)
-    d.line_follow_right_left(4500)
+    d.line_follow_right_left(2000)
     msleep(100)
     d.set_servo_position(c.LEFT_ARM, c.LA_FRONT)
 
 
 def back_to_up_ramp_position():
-    g.drive_timed(-50, 8500)
+    g.drive_timed(-80, 4000) #square up
     msleep(1000)
-    g.pivot_on_right_wheel(50, 90)
-    d.drive_timed(50, 50, 2000)
-    g.pivot_on_left_wheel(50, 90)
-
+    u.move_servo(c.LEFT_ARM, c.LA_BACK, 100)
+    g.drive_distance(50, 3)
+    #g.pivot_on_right_wheel(50, 90)
+    d.drive_timed(50, -50, 1500)
+    g.drive_distance(50, 12)
+    d.drive_timed(-50, 50, 1500)
+    g.drive_distance(50, 2)
 
 def go_up_ramp():
-    d.timed_line_follow_right_smooth(7300)
+    d.timed_line_follow_right_smooth(8200)
     msleep(100)
     # at the top of the ramp
-    g.drive_distance(-30, 2)
+    g.drive_distance(-30, 1.5)
     msleep(100)
     g.turn_with_gyro(0, 50, 90)
     msleep(100)
