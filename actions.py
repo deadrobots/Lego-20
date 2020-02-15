adcb49c82f903469c06d871db7267df7617c8a9b
Lexie <charlie.clyne@yahoo.com>
2020-02-15 17:00:49 -0500

    fixed prime from start to pom grab, cleaned up pom grab

--- actions.py
+++ actions.py
@@ -6,13 +6,30 @@ import constants as c
 
 
 def init():
+    if c.IS_PRIME:
+        print("I am lego prime")
+    else:
+        print("I am lego clone")
     print("running init")
     enable_servos()
     print("don't touch me, i'm calibrating!!")
     g.calibrate_gyro()
-    # power_on_test()
+    #power_on_test()
     u.move_servo(c.LEFT_ARM, c.LA_FRONT)
+    u.move_servo(c.FRONT_ARM, c.FA_UP)
+    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
+
+
+
+    # for start at top of ramp
+    u.move_servo(c.LEFT_ARM, c.LA_SIDE)
+    u.move_servo(c.FRONT_ARM, c.FA_MID)
+    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED)
+
+
+
     u.wait_for_button()
+    c.START_TIME = seconds()
 
 
 def power_on_test():
@@ -38,10 +55,6 @@ def power_on_test():
     g.pivot_on_left_wheel(30, 15)
     g.pivot_on_left_wheel(-30, 15)
     g.drive_timed(-30, 1000)
-    if c.IS_PRIME:
-        print("i am lego prime")
-    else:
-        print("i am lego clone")
     # testing tophat
     print("testing right tophat")
     g.drive_condition(50, d.on_white_right)
@@ -84,15 +97,20 @@ def move_coupler_to_blocks():
 
 
 def back_to_up_ramp_position():
-    g.drive_timed(-80, 4000) #square up
-    msleep(1000)
+    g.drive_timed(-90, 4000) # square up
     u.move_servo(c.LEFT_ARM, c.LA_BACK, 100)
-    g.drive_distance(50, 3)
-    #g.pivot_on_right_wheel(50, 90)
-    d.drive_timed(50, -50, 1500)
-    g.drive_distance(50, 12)
-    d.drive_timed(-50, 50, 1500)
-    g.drive_distance(50, 2)
+    g.drive_distance(70, 3)
+    if c.IS_PRIME:
+        d.drive_timed(50, -50, 1500)
+        g.drive_distance(70, 13)
+        d.drive_timed(-50, 50, 1500)
+    else:
+        d.drive_timed(50, -50, 1600)
+        g.drive_distance(70, 13)
+        d.drive_timed(-50, 50, 1600)
+    g.drive_distance(70, 2)
+    set_servo_position(c.LEFT_ARM, c.LA_SIDE)  # fold in left arm
+
 
 def go_up_ramp():
     d.timed_line_follow_right_smooth(8200)
@@ -102,6 +120,36 @@ def go_up_ramp():
     msleep(100)
     g.turn_with_gyro(0, 50, 90)
     msleep(100)
+    u.move_servo(c.FRONT_ARM, c.FA_MID)
+
+
+def go_to_center():
+    d.line_follow_until_switch()
+    g.drive_distance(-50, 4)
+    u.move_servo(c.FRONT_ARM, c.FA_DOWN1)
+    msleep(500)
+    u.move_servo(c.FRONT_CLAW, c.FC_OPEN_BIN, 5)
+    msleep(500)
+    g.drive_distance(25, .6)
+    msleep(500)
+    u.move_servo(c.FRONT_ARM, c.FA_DOWN2, 5)
+    msleep(500)
+    g.drive_distance(25, .6)
+    msleep(500)
+    u.move_servo(c.FRONT_ARM, c.FA_DOWN3, 5)
+    msleep(500)
+    u.move_servo(c.FRONT_CLAW, c.FC_CLOSED_BIN, 5)
+    msleep(500)
+    u.move_servo(c.FRONT_ARM, c.FA_DOWN2, 3)
+    msleep(500)
+    g.drive_distance(-25, .6)
+    msleep(500)
+    u.move_servo(c.FRONT_ARM, c.FA_DOWN1, 3)
+    msleep(500)
+    g.drive_distance(-25, .6)
+    msleep(500)
+    u.move_servo(c.FRONT_ARM, c.FA_MID, 3)
+    u.DEBUG()
 
 
 def reverse_and_turn():
