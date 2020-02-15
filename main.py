d1c3ea6aa46f7d9dc80fd0316b1042540eb6f93c
Lexie <charlie.clyne@yahoo.com>
2020-02-14 21:37:03 -0500

    consolidated init, cleaned up the drive to bin, drop bin, and ramp drive.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
     a.back_to_up_ramp_position()
     set_servo_position(c.LEFT_ARM, c.LA_SIDE)  # fold in left arm
     a.go_up_ramp()
+    u.DEBUG()
     d.line_follow_until_switch()
     # u.move_servo(c.RIGHT_ARM, c.RA_DOWN) #lower long arm into pit
     u.wait_for_button()
