f267036fc89ebadd9a4f0ea81e322a447049b5f8
Lexie <charlie.clyne@yahoo.com>
2020-02-15 17:01:44 -0500

    fixed prime from start to pom grab, cleaned up pom grab

--- main.py
+++ main.py
@@ -11,15 +11,11 @@ import constants as c
 
 def main():
     a.init()
-    c.START_TIME = seconds()
-    a.lower_ramp()
-    a. move_coupler_to_blocks()
-    a.back_to_up_ramp_position()
-    set_servo_position(c.LEFT_ARM, c.LA_SIDE)  # fold in left arm
-    a.go_up_ramp()
-    u.DEBUG()
-    d.line_follow_until_switch()
-    # u.move_servo(c.RIGHT_ARM, c.RA_DOWN) #lower long arm into pit
+    # a.lower_ramp()
+    # a. move_coupler_to_blocks()
+    # a.back_to_up_ramp_position()
+    # a.go_up_ramp()
+    a.go_to_center()
     u.wait_for_button()
     a.reverse_and_turn()
     u.DEBUG()
